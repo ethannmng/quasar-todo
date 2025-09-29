5731acd4cbd7b508534353243478285fb515f081
ethannmng <ethanmng.pro@gmail.com>
2025-09-29 10:02:34 +0200

    Create Navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,98 +1,133 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+	<q-header>
+	  <q-toolbar>
+		<q-btn
+		  flat
+		  dense
+		  round
+		  icon="menu"
+		  aria-label="Menu"
+		  @click="toggleLeftDrawer"
+		/>
+	  </q-toolbar>
+	  <div class="q-px-lg q-pt-xl q-mb-md">
+		<div class="text-h3">Todo</div>
+		<div class="text-subtitle1">{{ todaysDate }}</div>
+	  </div>
+	  <q-img src="/public/montagnes.jpg" class="header-image absolute-top"/>
+	</q-header>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+	 <q-drawer
+		v-model="leftDrawerOpen"
+		show-if-above
+		:width="250"
+		:breakpoint="600"
+	  >
+		<q-scroll-area style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd">
+		  	<q-list padding>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+				<q-item to="/" exact clickable v-ripple>
+			  		<q-item-section avatar>
+						<q-icon name="liste"/>
+			  		</q-item-section>
+			  		<q-item-section>To-do</q-item-section>
+				</q-item>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+				<q-item to="/help" exact clickable v-ripple>
+			  		<q-item-section avatar>
+						<q-icon name="help"/>
+			  		</q-item-section>
+			  		<q-item-section>Aide</q-item-section>
+				</q-item>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+		  </q-list>
+		</q-scroll-area>
+
+		<q-img class="absolute-top" src="public/montagnes.jpg" style="height: 192px">
+		  	<div class="absolute-bottom bg-transparent">
+				<q-avatar size="56px" class="q-mb-sm">
+			  		<img src="https://avatars.githubusercontent.com/u/146804884?v=4">
+				</q-avatar>
+				<div class="text-weight-bold">Ethan MÉNAGÉ</div>
+				<div>@ethannmng</div>
+		  </div>
+		</q-img>
+	  </q-drawer>
+
+	<q-page-container>
+		<KeepAlive>
+			<router-view />
+		</KeepAlive>
+	</q-page-container>
   </q-layout>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { date } from 'quasar'
+import { ref, computed } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+	title: 'Docs',
+	caption: 'quasar.dev',
+	icon: 'school',
+	link: 'https://quasar.dev'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+	title: 'Github',
+	caption: 'github.com/quasarframework',
+	icon: 'code',
+	link: 'https://github.com/quasarframework'
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+	title: 'Discord Chat Channel',
+	caption: 'chat.quasar.dev',
+	icon: 'chat',
+	link: 'https://chat.quasar.dev'
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+	title: 'Forum',
+	caption: 'forum.quasar.dev',
+	icon: 'record_voice_over',
+	link: 'https://forum.quasar.dev'
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+	title: 'Twitter',
+	caption: '@quasarframework',
+	icon: 'rss_feed',
+	link: 'https://twitter.quasar.dev'
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+	title: 'Facebook',
+	caption: '@QuasarFramework',
+	icon: 'public',
+	link: 'https://facebook.quasar.dev'
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+	title: 'Quasar Awesome',
+	caption: 'Community Quasar projects',
+	icon: 'favorite',
+	link: 'https://awesome.quasar.dev'
   }
 ]
+
+const todaysDate = computed(() => {
+  const days = [
+	'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'
+  ]
+  const months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
+  
+  const date = new Date()
+  const dayName = days[date.getDay()]
+  const day = date.getDate()
+  const monthName = months[date.getMonth()]
+  const year = date.getFullYear()
+
+  return `${dayName} ${day} ${monthName} ${year}`
+})
 
 const leftDrawerOpen = ref(false)
 
@@ -100,3 +135,11 @@ function toggleLeftDrawer () {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
 </script>
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,9 @@
+<template>
+	<q-page class="flex flex-center">
+		<h5>To-do</h5>
+	</q-page>
+</template>
+
+<script setup>
+
+</script>
